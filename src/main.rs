3f0606c0b46a66d3864a9551e260a8bbdaedf687
Sumeet Agarwal <sumeet.a@gmail.com>
2021-08-20 21:16:22 -0700

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use itertools::Itertools;
+use std::iter::Peekable;
+use std::slice::Iter;
 
 #[derive(Debug)]
 pub struct Program {
@@ -8,26 +10,41 @@ pub struct Program {
 #[derive(Debug)]
 enum Expr {
     Comment(String),
+    Assignment(Assignment),
 }
 
-fn assemble_program(program: &Program) -> String {
-    let mut assembled = "".to_owned();
+#[derive(Debug)]
+struct Assignment {
+    name: String,
+    expr: Box<Expr>,
+}
+
+fn assemble_program(program: &Program, assembled: &mut String) {
     let mut exprs = program.exprs.iter().peekable();
     while let Some(expr) = exprs.next() {
-        match expr {
-            Expr::Comment(ref body) => {
-                for line in body.lines() {
-                    assembled.push_str("// ");
-                    assembled.push_str(line);
-                    assembled.push_str("\n");
-                }
-                if let Some(Expr::Comment(_)) = exprs.peek() {
-                    assembled.push_str("\n");
-                }
+        assemble_expr(assembled, &mut exprs, expr)
+    }
+}
+
+fn assemble_expr(assembled: &mut String, exprs: &mut Peekable<Iter<Expr>>, expr: &Expr) {
+    match expr {
+        Expr::Comment(ref body) => {
+            for line in body.lines() {
+                assembled.push_str("// ");
+                assembled.push_str(line);
+                assembled.push_str("\n");
+            }
+            if let Some(Expr::Comment(_)) = exprs.peek() {
+                assembled.push_str("\n");
             }
         }
+        Expr::Assignment(Assignment { name, expr }) => {
+            assembled.push_str("let ");
+            assembled.push_str(name);
+            assembled.push_str(" = ");
+            assemble_expr(assembled, exprs, expr);
+        }
     }
-    assembled
 }
 
 peg::parser! {
@@ -36,16 +53,20 @@ peg::parser! {
         = _ exprs:(expr() ** _) _  { Program { exprs } }
 
     rule expr() -> Expr
-        = comment()
+        = comment() / assignment()
+
+    rule assignment() -> Expr
+        = "let" _ ident:ident() _ "=" _ expr:expr() { Expr::Assignment(Assignment {
+            name: ident.to_string(),
+            expr: Box::new(expr),
+        })}
+
 
     rule comment() -> Expr = c:comment_string() { Expr::Comment(c) }
-
     rule comment_string() -> String
         = "/" "/" _? body:$([^ '\r' | '\n']*)? following:following_comment()*  {
-            let c = body.map(|b| b.to_owned()).into_iter().chain(following.into_iter()).join(" ");
-            c
+            body.map(|b| b.to_owned()).into_iter().chain(following.into_iter()).join(" ")
         }
-
     rule following_comment() -> String
         = newline()? c:comment_string() {
             if c.starts_with("//") {
@@ -55,6 +76,9 @@ peg::parser! {
                 c
             }
         }
+
+    rule ident() -> &'input str = $(ident_start()+ ['a'..='z' | 'A'..='Z' | '_' | '0'..='9']*)
+    rule ident_start() -> &'input str = $(['a'..='z' | 'A'..='Z' | '_']+)
 
     rule nbspace() = [' ' | '\t']+
     rule newline() = "\n" / "\r\n"
@@ -70,10 +94,14 @@ pub fn main() -> anyhow::Result<()> {
 //
 // this is the continuation
 
+let x123 = // hello
+
 
 // this is a different comment
 "#;
     let program = list_parser::program(input)?;
-    println!("{}", assemble_program(&program));
+    let mut assembled = String::new();
+    assemble_program(&program, &mut assembled);
+    println!("{}", assembled);
     Ok(())
 }
